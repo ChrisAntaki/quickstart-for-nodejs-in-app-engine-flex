901dbb142c4f77680944d2720f987c8c5998f0d9
Chris Antaki <ChrisAntaki@gmail.com>
2018-02-03 11:29:27 -0800

    Updated default port to 80

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
